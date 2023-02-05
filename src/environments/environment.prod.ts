47dd109de9b66ed7548c54e347e3a24654656568
huyerve <mrhuk06@gmail.com>
2023-02-05 21:57:00 +0400

    added build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox: {
+    accessToken: 'pk.eyJ1Ijoiem9vbW5payIsImEiOiJjbGRyZGhvbXIxa2c0M3ZxZ2xvdGwzMHo5In0.xRY-iHH4ZrgO24zscCL0dg'
+  }
 };
